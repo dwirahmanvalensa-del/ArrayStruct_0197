8fe50aca70318a3d6f585aa528dcfa1b7509cebe
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-04-06 16:09:40 +0800

    menambahkan logika input dan menampilkan hasil data mhs

--- struct2.cpp
+++ struct2.cpp
@@ -14,5 +14,22 @@ struct Orang
     int umur;
 };
 
+int main() {
+    Orang mhs;
+    cout << "Nama = ";
+    cin >> mhs.nama;
+    cout << "Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Kota = ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur = ";
+    cin >> mhs.umur;
+    cout << endl;
+    // tampilkan
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Desa : " << mhs.alamat.desa << endl;
+    cout << "Kota : " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
 
+}
 
