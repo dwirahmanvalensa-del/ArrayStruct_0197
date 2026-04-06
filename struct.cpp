973c5160972b406fbcda675fe4299e2e2556e6aa
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-04-06 16:06:25 +0800

    menambahkan logika input dan menampilkan data

--- struct.cpp
+++ struct.cpp
@@ -8,5 +8,19 @@ struct Orang
     int umur;
 };
 
+int main() {
+    Orang mhs;
+    cout << "Nama = ";
+    cin >> mhs.nama;
+    cout << "Alamat = ";
+    cin >> mhs.alamat;
+    cout << "Umur = ";
+    cin >> mhs.umur;
+    cout << endl;
+    // tampilkan
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
 
+}
 
